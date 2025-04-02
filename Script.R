7b29efba6e7a47621d170bf24e80204cc3cb8236
Jose <jduvmx@gmail.com>
2025-04-01 20:36:52 -0600

    Make sure columns are numeric

--- Script.R
+++ Script.R
@@ -1,3 +1,9 @@
 # Importar datos
 library(dplyr)
 data <- read.csv("NBAdraft.csv")
+# Asegurarnos de que las columnas sean numéricas y limpiar datos
+data <- data %>%
+  mutate(
+    bench = as.numeric(gsub("-", NA, bench)),     # Convertir bench a numérico
+    shuttle = as.numeric(gsub("-", NA, shuttle)) # Convertir shuttle a numérico
+  )
